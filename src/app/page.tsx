b68e5fd15822fbab7263a4b96cad69a1f193f6f5
AmandaLoureiro28 <amandaloureiro1010@gmail.com>
2025-04-15 17:06:47 -0300

    seção adicionada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Contacts from "./components/Contacts";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/skills";
+import Sobre from "./components/Sobre"
+
 
 
 
@@ -12,6 +14,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <Sobre />
       <Skills />
       <Projects />
       <Contacts />
